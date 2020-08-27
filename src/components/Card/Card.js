da5cb26a2f9259af692ad416bc6c27f5bfe7464c
Kazi Mashry <kazimashry@gmail.com>
2020-08-27 08:09:34 +0600

    course card completed

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 const Card = (props) => {
+    const { courseName, price, instructor, img } = props.course;
     return (
-        <div>
-            <div className="card" style={{width: "18rem"}}>
-                <img src={props.course.img} className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <button className="btn btn-primary">Buy This Course</button>
-                    </div>
+       <div class="card mb-4">
+            <img src={img} className="card-img-top img-fluid" alt="..." style={{height:"300px"}}/>
+            <div class="card-body">
+                <h2 className="card-title">{courseName}</h2>
+                <p className="card-text">By: {instructor}</p>
+                <p className="card-text"><small>Price: ${price}</small></p>
+            </div>
+            <div class="card-footer">
+                <button className="btn btn-primary">Buy This Course</button>
             </div>
         </div>
     );
--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import courseInfo from '../../DataBase/Database'
 import Card from '../Card/Card';
+import './Courses.css';
 
 const Courses = () => {
     console.table(courseInfo);
     let key = 1;
     return (
-        <div>
-            <div className="showCourses">
+        <div className="container">
+            <div className="card-deck">
                 {
                     courseInfo.map(course => <Card course={course} key={key++}></Card>)
                 }

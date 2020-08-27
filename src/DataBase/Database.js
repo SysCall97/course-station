d5d04b0197844735a2cc255a4aa2769830d214e3
Kazi Mashry <kazimashry@gmail.com>
2020-08-27 06:29:53 +0600

    database added

---
+++ src/DataBase/Database.js
@@ -0,0 +1,69 @@
+var courseInfo = [
+    {
+        courseName: "Web Development",
+        price: 100,
+        instructor: "Jhankar Mahbub"
+    },
+    {
+        courseName: "Mobile Application Development",
+        price: 105,
+        instructor: "Leanne Graham"
+    },
+    {
+        courseName: "Networking",
+        price: 110,
+        instructor: "Ervin Howell"
+    },
+    {
+        courseName: "Machine Learning",
+        price: 115,
+        instructor: "Clementine Bauch"
+    },
+    {
+        courseName: "DevOps",
+        price: 150,
+        instructor: "Patricia Lebsack"
+    },
+    {
+        courseName: "Basic Programming",
+        price: 80,
+        instructor: "Chelsey Dietrich"
+    },
+    {
+        courseName: "Linear Algebra",
+        price: 95,
+        instructor: "Kurtis Weissnat"
+    },
+    {
+        courseName: "Discrete Mathematics",
+        price: 100,
+        instructor: "Mrs. Dennis Schulist"
+    },
+    {
+        courseName: "Probability",
+        price: 90,
+        instructor: "Kurtis Weissnat"
+    },
+    {
+        courseName: "Optical Physics",
+        price: 100,
+        instructor: "Nicholas Runolfsdottir V"
+    },
+    {
+        courseName: "Operating System",
+        price: 90,
+        instructor: "Glenna Reichert"
+    },
+    {
+        courseName: "Robotics",
+        price: 70,
+        instructor: "Clementina DuBuque"
+    },
+    {
+        courseName: "Software Engineering",
+        price: 120,
+        instructor: "Leanne Graham"
+    }
+];
+
+export default courseInfo;
